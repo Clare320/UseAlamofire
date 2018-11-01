00fb0eaa01ecb9cce6da4b11d27da2dd6a330dd1
llj <979520102@qq.com>
2018-11-01 18:08:18 +0800

    Clare320
    添加一些操作符使用

--- UseAlamofire/OperatorViewController.swift
+++ UseAlamofire/OperatorViewController.swift
@@ -21,7 +21,63 @@ class OperatorViewController: UIViewController {
         // Do any additional setup after loading the view.
         
         testOperator()
+        testTransformElementOperator()
     }
+    
+    func testTransformElementOperator() -> Void {
+        let tmpArray = [1, 2, 3, 4, 5]
+        let resultArray = tmpArray.map {
+            return $0 * 2
+        }
+        print(resultArray)
+        
+        // compactMap与map区别--> compactMap会过滤空值
+        let flatMapArray = tmpArray.compactMap {
+            return $0 * 3
+        }
+        print(flatMapArray)
+        
+        /*--转换元素--*/
+        let subject = PublishSubject<Int>()
+        let subject2 = PublishSubject<Int>()
+        
+        // map
+        subject.map {
+            return String(describing: $0) + "Hello"
+            }.subscribe(onNext: { (item) in
+                print("after map --> \(item)")
+            }).disposed(by: disposeBag)
+    
+        // flatMap  第一次不会用 -- 咋用啊 将Observable中元素转化为
+        
+       
+//        subject.flatMap(<#T##selector: (Int) throws -> ObservableConvertibleType##(Int) throws -> ObservableConvertibleType#>)
+        
+        
+        let flatMapObservable = Observable.of(subject)
+        
+        flatMapObservable.flatMap { e in
+            return Observable<String>.create({ (observer) -> Disposable in
+                observer.onNext("Hello FlatMap \(e)")
+                observer.onCompleted()
+                
+                return Disposables.create()
+            })
+            }.subscribe(onNext: { (element) in
+                print("Flatmap ---> \(element)")
+            }).disposed(by: disposeBag)
+        
+        
+        subject.onNext(999)
+        subject.onNext(666)
+        subject.onNext(333)
+        
+        flatMapObservable.subscribe(onNext: { (e) in
+            print("flatMapObservable-->\(e)")
+        }).disposed(by: disposeBag)
+    
+    }
+    
     
     func testOperator() -> Void {
         // jsut 创建特定元素的序列
@@ -81,19 +137,49 @@ class OperatorViewController: UIViewController {
         
         // of 可以创建一个可变数量元素的Observable
         
+        
+        /*----通过组合其它的Observable来创建Observable*/
+        
         let subject1 = PublishSubject<String>()
         let subject2 = PublishSubject<String>()
         
+        // merge 将多个observable合并
         Observable.merge([subject1, subject2]).subscribe(onNext: { (item) in
             print("Merge---> \(item)")
         }).disposed(by: disposeBag)
         
+        // contact --> 按加入顺序将Observable串联起来，前一个发送完毕后一个才可以开始发送（前一个没完成后一个即使产生也不会被发送）
+        subject1.concat(subject2).subscribe {
+            print($0)
+        }.disposed(by: disposeBag)
+        
+        // zip  --- 严格按照序列索引数组合 新组合的Observable的元素数量等于源Observable中数量最少的那个。
+        Observable.zip(subject1, subject2) {
+                $0 + $1 + "we are zip"
+            }.subscribe(onNext: {
+                print("zip---> \($0)")
+            }).disposed(by: disposeBag)
+        
+        
+        // combineLatest --> 源中任何一个Observable发出元素，新的Observable都会发出一个（前提是源中Observablez曾经发出过）
+        Observable.combineLatest(subject1, subject2) {
+            "--CombineLatest--" + $0 + $1
+            }.subscribe(onNext: {
+                print("CombineLatest--->\($0)")
+            }).disposed(by: disposeBag)
+        
+        
+        // scan --> 将前一个元素按闭包处理后的结果当成后一个元素处理的参数 类似于Array中reduce
+        
+        
+        
+        
         subject1.onNext("Github")
         subject2.onNext("Swift")
         subject1.onNext("juejin")
+        subject1.onCompleted()
         subject2.onNext("javascript")
-        
-        
+        subject2.onNext("Python")
         
     }
     
